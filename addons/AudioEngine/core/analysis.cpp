1f06f6a33ad8e94e9473772c5e424fb63de055cc
Marek Bereza <bereza@gmail.com>
2012-02-06 17:30:24 +0000

    Bit of work on kinecting and sound

---
+++ addons/AudioEngine/core/analysis.cpp
@@ -0,0 +1,7 @@
+/**
+ *  AudioAnalysis.cpp
+ *
+ *  Created by Marek Bereza on 06/02/2012.
+ */
+
+#include "AudioAnalysis.h"
---
+++ apps/wail/WailWell/src/testAppVision.cpp
@@ -0,0 +1,7 @@
+/**
+ *  testAppVision.cpp
+ *
+ *  Created by Marek Bereza on 06/02/2012.
+ */
+
+#include "testAppVision.h"
