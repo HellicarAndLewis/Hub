7a2b1d3217b9c1fc9681a9b64e928f648b1984a4
Joel Gethin Lewis <joelgethinlewis@gmail.com>
2012-02-22 18:42:50 +0000

    Pango experimentation

---
+++ addons/ofxPango/src/ofxPCSurface.h
@@ -0,0 +1,23 @@
+#ifndef OFXPCSURFACEH
+#define OFXPCSURFACEH
+
+#include <pango/pangocairo.h>
+#include <string> // tmp
+class ofxPCSurface {
+public:
+	ofxPCSurface(
+				 float fWidth
+				 ,float fHeight
+				 ,cairo_format_t nType = CAIRO_FORMAT_ARGB32
+				 );
+	~ofxPCSurface();
+	int getWidth();
+	int getHeight();
+		//	unsigned char* getPixels();
+	uint32_t* getData();
+	void ref(std::string s = "");
+	cairo_surface_t* cr_surface;
+private:
+	
+};
+#endif
